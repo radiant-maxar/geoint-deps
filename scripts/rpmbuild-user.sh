2c21cc874624a949cafe8e226843f435f1daedc2
Justin Bronn <justin.bronn@maxar.com>
2021-03-04 11:22:05 -0700

    Add some comments and don't force a unique GID to accomodate our macOS friends.

--- scripts/rpmbuild-user.sh
+++ scripts/rpmbuild-user.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 # Add unprivileged user for building RPMs.
-groupadd -g "${RPMBUILD_GID}" "${RPMBUILD_GROUP}"
+groupadd --non-unique -g "${RPMBUILD_GID}" "${RPMBUILD_GROUP}"
 useradd -d "${RPMBUILD_HOME}" -m \
         -s "${RPMBUILD_SHELL:-/bin/bash}" \
         -u "${RPMBUILD_UID}" \
@@ -17,10 +17,13 @@ EOF
 
 # RPM macros.
 cat > "${RPMBUILD_HOME}/.rpmmacros" <<EOF
+# Allow rpmbuild to download source archives.
 %_disable_source_fetch %{nil}
+# RPM GPG signing settings.
 %_gpg_digest_algo ${RPMBUILD_GPG_DIGEST_ALGO:-sha512}
 %_gpg_name ${RPMBUILD_NAME}
 %_gpg_path ${RPMBUILD_GPG_PATH:-${RPMBUILD_HOME}/.gnupg}
+# Enable hardened builds by default.
 %_hardened_build 1
 %_topdir ${RPMBUILD_HOME}
 %debug_package %{nil}
