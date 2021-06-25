radiant-maxar/geoint-deps

commit aa77521ca4bdb79b2351c85a7a9cb33858e37ec4

Use `append` for the argument action, as `extend` is only supported on Python 3.8+.

scripts/buildrequires.py
@@ -13,7 +13,7 @@ def main():
     )
     parser.add_argument(
         "--define",
-        action="extend",
+        action="append",
         dest="defines",
         nargs="+",
         help="RPM macro definitions to use.",

commit de19fc6dbd955386ab5a1034219be69dd3bf26ac

Fix regression introduced by aa77521c.

scripts/buildrequires.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import argparse
+import itertools
 import json
 import sys
 
@@ -31,7 +32,10 @@ def main():
     build_defines = {}
     try:
         if args.defines:
-            build_defines = dict(define.split("=") for define in args.defines)
+            build_defines = dict(
+                define.split("=")
+                for define in itertools.chain.from_iterable(args.defines)
+            )
     except Exception as err:
         sys.stderr.write("Error: {}\n".format(err))
         sys.exit(2)